ef7158a25984168a1e940c500cb1ecb6df11dfea
Timmy Loos <122990491+timmyloos@users.noreply.github.com>
2025-05-24 02:20:16 -0400

    Add files via upload

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
+// import Link from "next/link"; // Removed unused import
 import SmoothScrollLink from "./SmoothScrollLink";
 
 export default function Navbar() {
